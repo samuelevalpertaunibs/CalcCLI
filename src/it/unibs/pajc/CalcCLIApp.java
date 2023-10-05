84021cb13fe17f7d79d5e0b8dd935e389939a426
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-05 15:21:16 +0200

    Refactor eval method and make adjustment in CalcCLIApp
    Changed private method name from `eval_internal` to `_eval` to conform to the team's naming convention for private methods in CalcModel.java. Also increased arithmetic complexity and adjusted the output format in CalcCLIApp.java accordingly. This provides more diverse test cases and makes the result more clearly displayed to the user.

--- src/it/unibs/pajc/CalcCLIApp.java
+++ src/it/unibs/pajc/CalcCLIApp.java
@@ -5,8 +5,8 @@ public class CalcCLIApp {
 	public static void main(String[] args) {
 		CalcModel model = new CalcModel();
 
-		model.pushOperand(1.);
 		model.pushOperand(5.);
+		model.pushOperand(3.);
 		model.pushOperand(2.);
 		model.pushOperator("+");
 		model.pushOperator("*");
