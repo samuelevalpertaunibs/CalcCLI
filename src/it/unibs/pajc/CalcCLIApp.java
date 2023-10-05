db6de24f73629ea67f1dcc5fd3017d0f4ac0d352
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-05 14:43:03 +0200

    Initial commit

---
+++ src/it/unibs/pajc/CalcCLIApp.java
@@ -0,0 +1,16 @@
+package it.unibs.pajc;
+
+public class CalcCLIApp {
+
+	public static void main(String[] args) {
+		CalcModel model = new CalcModel();
+
+		model.pushOperand(5.);
+		model.pushOperand(2.);
+		model.pushOperator("+");
+
+		System.out.println(model.dump());
+
+	}
+
+}
---
+++ src/it/unibs/pajc/CalcModel.java
@@ -0,0 +1,72 @@
+package it.unibs.pajc;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class CalcModel {
+	private HashMap<String, OperatoreBinario> operators = new HashMap<>();
+	
+	public CalcModel() {
+		operators.put("+", (x, y) -> x + y);
+		operators.put("-", (x, y) -> x - y);
+		operators.put("*", (x, y) -> x * y);
+		operators.put("/", (x, y) -> x / y);
+	}
+
+	public String listOperators() {
+		return null; // deve restituire una stringa contenente gli operatori gestiti
+	}
+	
+	// --------------------------------------------
+	// Stack notazione polacca inversa
+	// --------------------------------------------	
+	private class CompEl {
+		final Double value;
+		final OperatoreBinario operator;
+		final String operatorSymbol;
+		
+		CompEl(Double value) {
+			this.value = value;
+			this.operator = null;
+			this.operatorSymbol = null;
+		}
+		
+		CompEl(OperatoreBinario operator, String operatorSymbol) {
+			this.value = null;
+			this.operator = operator;
+			this.operatorSymbol = operatorSymbol;
+		}
+		
+		boolean isOperator() { return operator != null; }
+		
+		boolean isOperand() { return value != null; }
+		
+		public String toString() {
+			return isOperand() ? Double.toString(value) : operatorSymbol;
+		}	
+	}
+	
+	
+	private Stack<CompEl> compStack = new Stack<>();
+	
+	public void pushOperator(String operatorSymbol) {
+		OperatoreBinario op = operators.get(operatorSymbol);
+		if(op == null)
+			return;
+		compStack.add(new CompEl(op, operatorSymbol));
+		
+		
+		CompEl c = new CompEl(op, operatorSymbol);
+	}
+	
+	public void pushOperand(Double value) {
+		compStack.add(new CompEl(value));
+	}
+	
+	public String dump() {
+		return compStack.toString();
+		// restituire una stringa contente l'espressione data in input
+	}
+	
+	
+}
